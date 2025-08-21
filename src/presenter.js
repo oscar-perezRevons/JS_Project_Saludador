77eac8d6134b8cf72c5debd8324b3d822974f425
oscar-perezRevons <oscarsamuelperezcallizaya@gmail.com>
2025-08-21 09:17:16 -0400

    fetch: Add functionality language user

--- src/presenter.js
+++ src/presenter.js
@@ -1,35 +1,48 @@
 const nombreUsuario = document.querySelector("#nombre-usuario");
 const generoUsuario = document.querySelector("#genero-usuario");
 const edadUsuario = document.querySelector("#edad-usuario");
+const idiomaUsuario = document.querySelector("#idioma-usuario");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#form-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
   const nombre = nombreUsuario.value;
   const genero = generoUsuario.value;
   const edad = parseInt(edadUsuario.value, 10);
+  const idioma = idiomaUsuario.value;
   const hora = new Date().getHours();
 
-  let saludoHora;
+  let saludoHoraEs, saludoHoraEn;
   if (hora >= 6 && hora < 12) {
-    saludoHora = "Buenos días";
+    saludoHoraEs = "Buenos días";
+    saludoHoraEn = "Good morning";
   } else if (hora >= 12 && hora < 20) {
-    saludoHora = "Buenas tardes";
+    saludoHoraEs = "Buenas tardes";
+    saludoHoraEn = "Good afternoon";
   } else {
-    saludoHora = "Buenas noches";
+    saludoHoraEs = "Buenas noches";
+    saludoHoraEn = "Good evening";
   }
 
-  let prefijo = "";
+  let prefijoEs = "", prefijoEn = "";
   if (edad > 30) {
     if (genero === "masculino") {
-      prefijo = "Sr.";
+      prefijoEs = "Sr.";
+      prefijoEn = "Mr.";
     } else if (genero === "femenino") {
-      prefijo = "Sra.";
-    } else {
-      prefijo = "";
+      prefijoEs = "Sra.";
+      prefijoEn = "Mrs.";
     }
   }
 
-  div.innerHTML = `<p>${saludoHora} ${prefijo} ${nombre}</p>`;
+  let saludoFinal = "";
+  if (idioma === "es") {
+    saludoFinal = `<p>${saludoHoraEs} ${prefijoEs} ${nombre}</p>`;
+  } else {
+    saludoFinal = `<p>${saludoHoraEn} ${prefijoEn} ${nombre}</p>`;
+  }
+
+  div.innerHTML = saludoFinal;
 });
