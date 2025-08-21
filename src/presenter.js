c266114fe5615ffdf0e2ec2a3f1adec04a5a4dbc
oscar-perezRevons <oscarsamuelperezcallizaya@gmail.com>
2025-08-21 08:59:10 -0400

    fetch: Add functionality gender user

--- src/presenter.js
+++ src/presenter.js
@@ -1,20 +1,28 @@
 const nombreUsuario = document.querySelector("#nombre-usuario");
+const generoUsuario = document.querySelector("#genero-usuario");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#form-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const nombre = nombreUsuario.value;
+  const genero = generoUsuario.value;
   const hora = new Date().getHours();
 
-  let saludo;
+  let saludoHora;
   if (hora >= 6 && hora < 12) {
-    saludo = "Buenos días";
+    saludoHora = "Buenos días";
   } else if (hora >= 12 && hora < 20) {
-    saludo = "Buenas tardes";
+    saludoHora = "Buenas tardes";
   } else {
-    saludo = "Buenas noches";
+    saludoHora = "Buenas noches";
   }
 
-  div.innerHTML = `<p>${saludo} ${nombre}</p>`;
+  let saludoGenero;
+  if (genero === "masculino") {
+    saludoGenero = "señor";
+  } else if (genero === "femenino") {
+    saludoGenero = "señora";
+  }
+  div.innerHTML = `<p>${saludoHora} ${saludoGenero} ${nombre}</p>`;
 });
