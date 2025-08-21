0fdd3f1bed09e0408fd290fb78a24638efdd0f7b
oscar-perezRevons <oscarsamuelperezcallizaya@gmail.com>
2025-08-21 08:55:27 -0400

    feat: Add hour

--- src/presenter.js
+++ src/presenter.js
@@ -4,5 +4,17 @@ const div = document.querySelector("#form-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  div.innerHTML = "<p>Hola " + nombreUsuario.value + "</p>";
+  const nombre = nombreUsuario.value;
+  const hora = new Date().getHours();
+
+  let saludo;
+  if (hora >= 6 && hora < 12) {
+    saludo = "Buenos días";
+  } else if (hora >= 12 && hora < 20) {
+    saludo = "Buenas tardes";
+  } else {
+    saludo = "Buenas noches";
+  }
+
+  div.innerHTML = `<p>${saludo} ${nombre}</p>`;
 });
